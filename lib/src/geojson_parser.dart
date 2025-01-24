42da9ca89f37943521a4e549a6d6e09c692b6548
Nafi Alfanthariq <naffripperz@gmail.com>
2025-01-24 10:25:48 +0700

    fix geojson_parser

--- lib/src/geojson_parser.dart
+++ lib/src/geojson_parser.dart
@@ -304,10 +304,10 @@ class GeoJsonParser {
                   if (pathIndex == 0) {
                     // add to polygon's outer ring
                     outerRing
-                        .add(LatLng((coords[1] as num).toDouble(), (coords[0] as num).toDouble())););
+                        .add(LatLng((coords[1] as num).toDouble(), (coords[0] as num).toDouble())));
                   } else {
                     // add it to a hole
-                    hole.add(LatLng((coords[1] as num).toDouble(), (coords[0] as num).toDouble())););
+                    hole.add(LatLng((coords[1] as num).toDouble(), (coords[0] as num).toDouble())));
                   }
                 }
                 if (pathIndex > 0) {
